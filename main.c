3b8f2beaaec75603da60fc9e11e8017cc3213a8f
ybouhjira <b.youssef91@gmail.com>
2014-01-19 00:44:16 +0000

    Backup

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ typedef struct Entree
 typedef struct TableHashage
 {
   int size;
-  struct Entree *vals;
+  struct Entree **vals;
 }TableHashage;
 
 /** Nom de la fonction : tablehashage_creer
@@ -55,12 +55,11 @@ TableHashage tablehashage_creer(int size)
   * Description :
   *   convertit une cle en indice
   */
-unsigned int convertir_chaine(TableHashage *table, char *cle)
+unsigned int convertir_chaine(TableHashage table, char *cle)
 {
   unsigned int index;
   unsigned int i = 0;
 
-  /* Convert our string to an integer */
   while(index < ULONG_MAX && i < strlen(cle))
     {
       index = index << 8;
@@ -68,89 +67,105 @@ unsigned int convertir_chaine(TableHashage *table, char *cle)
       i++;
     }
 
-  return index % table->size;
+  return index % table.size;
 }
 
-/* Insert a key-value pair into a hash table. */
-void ht_set(TableHashage *hashtable, char *key, char *value) {
-  int bin = 0;
-  Entree *newpair = NULL;
-  Entree *next = NULL;
-  Entree *last = NULL;
+/* Create a key-value pair. */
+Entree *entree_creer(char *cle, int val) {
+  Entree *nouv;
 
-  bin = convertir_chaine(hashtable, key);
+  if(!(nouv = calloc(1, sizeof(Entree))))
+    {
+      printf("Erreur d'allocation dynamique\n");
+      exit(1);
+    }
+  strcpy(nouv->cle, cle);
+  nouv->val = val;
 
-  next = hashtable->vals[ bin ];
+  return nouv;
+}
 
-  while(next != NULL && next->cle != NULL && strcmp(key, next->cle) > 0) {
-      last = next;
-      next = next->suiv;
+/** Nom de la fonction :  tablehashage_inserer
+  * Entrées :
+  *   TableHashage table : Table de hashage
+  *   char *cle : La clé
+  *   int val : La valeur
+  * Description :
+  *   insere une valeur dans la table de hashage
+  */
+void ht_set(TableHashage table, char *cle, int val) {
+  assert(cle);
+
+  int indice = 0;
+  Entree *nouv = NULL;
+  Entree *suiv = NULL;
+  Entree *prec = NULL;
+
+  indice = convertir_chaine(table, cle);
+
+  suiv = table.vals[indice];
+
+  while(suiv && strcmp(cle, suiv->cle) > 0)
+    {
+      prec = suiv;
+      suiv = suiv->suiv;
     }
 
-  /* There's already a pair.  Let's replace that string. */
-  if(next != NULL && next->cle != NULL && strcmp(key, next->cle) == 0) {
+  // La clé existe deja
+  if(suiv && !strcmp(cle, suiv->cle))  suiv->val = val;
+  else
+    {
+      nouv = entree_creer(cle, val);
 
-      free(next->value);
-      next->value = strdup(value);
-
-      /* Nope, could't find it.  Time to grow a pair. */
-    } else {
-      newpair = ht_newpair(key, value);
-
-      /* We're at the start of the linked list in this bin. */
-      if(next == hashtable->vals[ bin ]) {
-          newpair->suiv = next;
-          hashtable->vals[ bin ] = newpair;
-
-          /* We're at the end of the linked list in this bin. */
-        } else if (next == NULL) {
-          last->suiv = newpair;
-
-          /* We're in the middle of the list. */
-        } else  {
-          newpair->suiv = next;
-          last->suiv = newpair;
+      // tete de la liste chainée
+      if(suiv == table.vals[indice])
+        {
+          nouv->suiv = suiv;
+          table.vals[indice] = nouv;
+        }
+      // fin de la liste chainée
+      else if (suiv == NULL) prec->suiv = nouv;
+      // milieu de la liste
+      else
+        {
+          nouv->suiv = suiv;
+          prec->suiv = nouv;
         }
     }
 }
 
 /* Retrieve a key-value pair from a hash table. */
-char *ht_get(TableHashage *hashtable, char *key) {
+int ht_get(TableHashage table, char *key) {
   int bin = 0;
   Entree *pair;
 
-  bin = convertir_chaine(hashtable, key);
+  bin = convertir_chaine(table, key);
 
   /* Step through the bin, looking for our value. */
-  pair = hashtable->vals[ bin ];
-  while(pair != NULL && pair->cle != NULL && strcmp(key, pair->cle) > 0) {
-      pair = pair->suiv;
-    }
+  pair = table.vals[bin];
+  while(pair && strcmp(key, pair->cle) > 0) pair = pair->suiv;
 
   /* Did we actually find anything? */
-  if(pair == NULL || pair->cle == NULL || strcmp(key, pair->cle) != 0) {
-      return NULL;
-
-    } else {
-      return pair->value;
-    }
-
+  if(!pair && strcmp(key, pair->cle) != 0)
+    return 0;
+  else
+    return pair->val;
 }
 
 
 int main()
 {
-  TableHashage *hashtable = tablehashage_creer(65536);
+  TableHashage hashtable = tablehashage_creer(65536);
 
-  ht_set(hashtable, "key1", "inky");
-  ht_set(hashtable, "key2", "pinky");
-  ht_set(hashtable, "key3", "blinky");
-  ht_set(hashtable, "key4", "floyd");
+  ht_set(hashtable, "key1", 1);
+  ht_set(hashtable, "key2", 2);
+  ht_set(hashtable, "key3", 5);
+  ht_set(hashtable, "key4", 4);
 
-  printf("%s\n", ht_get(hashtable, "key1"));
-  printf("%s\n", ht_get(hashtable, "key2"));
-  printf("%s\n", ht_get(hashtable, "key3"));
-  printf("%s\n", ht_get(hashtable, "key4"));
+  printf("%d\n", ht_get(hashtable, "key1"));
+  printf("%d\n", ht_get(hashtable, "key2"));
+  printf("%d\n", ht_get(hashtable, "key3"));
+  printf("%d\n", ht_get(hashtable, "key4"));
 
   return 0;
 }
